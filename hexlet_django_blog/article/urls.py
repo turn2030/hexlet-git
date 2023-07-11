ec06e2f531f051a16833fe0b15e577f0ec878b04
turn2030 <o.rogalin@gmail.com>
2023-07-11 16:05:40 +0300

    add base.html & about.html

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -2,4 +2,3 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [path('', views.index),]
-
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'articles.html', context={'name': 'articles',})
